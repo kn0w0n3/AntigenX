05797883c3500292e2ad531029d2d11f128df6eb
equati0n <tr14rc3@gmail.com>
2022-11-02 21:55:59 -0700

    Add scan URL -  fix buttons - VPN menu
    Add buttons:  quick scan, run in sand box, add signatures, VPN, Scan Url. Fix animations. Add VPN window. Still working out sqlite 3 bugs with linux

--- database.cpp
+++ database.cpp
@@ -1,212 +1,336 @@
-#include "database.h"
-
-Database::Database(QObject *parent) : QObject(parent){
-
-    mainWinDB = QSqlDatabase::addDatabase("QSQLITE", "conn1-MainWinInfo");
-    mainWinDB.setDatabaseName("/home/voldem0rt/Desktop/AntigenX-main/db/program_info.db");
-
-    //For now, do an initial check to see if the connections are working.
-    if(mainWinDB.open()){
-        qDebug() << "Constructor:  mainWinDB Connected!!!......";
-    }
-
-    /*
-    //Multiple connections were required to work with the different tables simultaneously.
-    //Format for the query: QSqlQuery query(QSqlDatabase::database("conn2-BillInfo"));
-    sEmailDB = QSqlDatabase::addDatabase("QSQLITE", "conn1_EmailInfo");
-    sEmailDB.setDatabaseName("C:/Users/Documents/Qt_Projects/Bill-Bot/database/bill_info.db");
-
-    billDB = QSqlDatabase::addDatabase("QSQLITE", "conn2-BillInfo");
-    billDB.setDatabaseName("C:/Users/Documents/Qt_Projects/Bill-Bot/database/bill_info.db");
-
-    populateDB = QSqlDatabase::addDatabase("QSQLITE", "conn3-popCReminders_");
-    populateDB.setDatabaseName("C:/Users/Documents/Qt_Projects/Bill-Bot/database/bill_info.db");
-
-    popEmailDB = QSqlDatabase::addDatabase("QSQLITE", "conn4-popSEmailBox");
-    popEmailDB.setDatabaseName("C:/Users/Documents/Qt_Projects/Bill-Bot/database/bill_info.db");
-
-    fEmailDB = QSqlDatabase::addDatabase("QSQLITE", "conn5-fEmailBox");
-    fEmailDB.setDatabaseName("C:/Users/Documents/Qt_Projects/Bill-Bot/database/bill_info.db");
-
-
-
-    //For now, do an initial check to see if the connections are working.
-    if(!sEmailDB.open()){
-        qDebug() << "Constructor:  sEmailDB Not Connected......";
-    }
-    else{
-        qDebug() << "Constructor:  sEmailDB is Connected.......";
-    }
-    if(!billDB.open()){
-        qDebug() << "Constructor: billDB Not Connected......";
-    }
-    else{
-        qDebug() << "Constructor: billDB is Connected.......";
-    }
-    if(!populateDB.open()){
-        qDebug() << "Constructor: populateDB Not Connected......";
-    }
-    else{
-        qDebug() << "Constructor: populateDB is Connected.......";
-    }
-    if(!popEmailDB.open()){
-        qDebug() << "Constructor: popEmailDB Not Connected......";
-    }
-    else{
-        qDebug() << "Constructor: popEmailDB is Connected.......";
-    }
-    counter = 0;
-    //getFirstDatabaseEntries();
-    */
-}
-
-void Database::getMainWinInfo(QString mwStatusF, QString mwlastFullScanF, QString mwLastSmartScanF, QString mwLastSingleScanF, QString mwLastUpdateF, QString mwThreatsFoundF, QString mwCurrentThreatsF){
-    mwStatus =  mwStatusF;
-    mwlastFullScan = mwlastFullScanF;
-    mwLastSmartScan = mwLastSmartScanF;
-    mwLastSingleScan = mwLastSingleScanF;
-    mwLastUpdate = mwLastUpdateF;
-    mwThreatsFound = mwThreatsFoundF;
-    mwCurrentThreats = mwCurrentThreatsF;
-}
-
-void Database::dbInsertMainWIn(){
-    if(!mainWinDB.open()){
-        qDebug() << "Database Not Connected..........";
-    }
-    else{
-        QSqlQuery query(QSqlDatabase::database("conn1-MainWinInfo"));
-        query.exec("INSERT INTO main-win-info (mw-status, mw-last-full-scan, mw-last-smart-scan, mw-last-single-scan, mw-last-update, mw-threats-found, mw-current-threats)" "VALUES (?, ?, ?, ?, ?, ?, ?)");
-        qDebug() << "Inserting info to database.......";
-        query.bindValue(0, mwStatus);
-        query.bindValue(1, mwlastFullScan);
-        query.bindValue(2, mwLastSmartScan);
-        query.bindValue(3, mwLastSingleScan);
-        query.bindValue(4, mwLastUpdate);
-        query.bindValue(5, mwThreatsFound);
-        query.bindValue(6, mwCurrentThreats);
-        query.exec();
-    }
-}
-/*
-//Receive and store bill information from the QML form
-void Database::receiveBillInfoFromQML(QString bName, QString dDate, QString dtNotify, QString aDue){
-    billName = bName;
-    dueDate = dDate;
-    dateToNotify = dtNotify;
-    amountDue = aDue;
-    insertBillDbInfo();
-}
-
-//Insert sender email info to the database.
-void Database::insertEmailToDb(){
-    if(!sEmailDB.open()){
-        qDebug() << "Function: Local Email Database Not Connected......";
-    }
-    else{
-        qDebug() << "Function: Local Email Info Database is Connected.......";
-        QSqlQuery insertEmailQuery(QSqlDatabase::database("conn1_EmailInfo"));
-        insertEmailQuery.exec("INSERT INTO sender_email_info (username, password, server, port_number) " "VALUES (?, ?, ?, ?)");
-
-        qDebug() << "Trying to insert email info to database.......";
-        insertEmailQuery.bindValue(0, iUsername);
-        insertEmailQuery.bindValue(1, iPassword);
-        insertEmailQuery.bindValue(2, iServer);
-        insertEmailQuery.bindValue(3, iPortNumber);
-        insertEmailQuery.exec();
-    }
-}
-
-//Insert reminder form info into the database.
-void Database::insertBillDbInfo(){
-    if(!billDB.open()){
-        qDebug() << "Database Not Connected..........";
-    }
-    else{
-        QSqlQuery query(QSqlDatabase::database("conn2-BillInfo"));
-        query.exec("INSERT INTO bill_info_table (db_bill_name, db_due_date, db_date_to_notify, db_amount_due)" "VALUES (?, ?, ?, ?)");
-        qDebug() << "Inserting info to database.......";
-        query.bindValue(0, billName);
-        query.bindValue(1, dueDate);
-        query.bindValue(2, dateToNotify);
-        query.bindValue(3, amountDue);
-        query.exec();
-    }
-}
-
-//Get the current saved reminders from the database and populate the main menu QML form.
-void Database::populateCurrentReminders(){
-    QSqlQuery popCReminders(QSqlDatabase::database("conn3-popCReminders_"));
-    popCReminders.exec("SELECT db_bill_name, db_due_date, db_date_to_notify, db_amount_due FROM bill_info_table WHERE id > 0");
-    qDebug() << "Pulling from database to populate current reminders............";
-    while(popCReminders.next()){
-        pBillName = popCReminders.value(0).toString();
-        qDebug() << "billName: " + pBillName.trimmed();
-        emit dbBillNameToQml(pBillName.trimmed() + "    |    ");
-
-        pDueDate = popCReminders.value(1).toString();
-        qDebug() << "dueDate: " + pDueDate.trimmed();
-        emit dbDueDateToQml(pDueDate.trimmed() + "    |    ");
-
-        pDateToNotify = popCReminders.value(2).toString();
-        qDebug() << "Notify Date: " + pDateToNotify.trimmed();
-        emit dbNotifyDateToQml(pDateToNotify.trimmed() + "    |    ");
-
-        pAmountDue = popCReminders.value(3).toString();
-        qDebug() << "Amount Due: " + pAmountDue.trimmed();
-        emit dbAmountDueToQml(pAmountDue.trimmed());
-    }
-    emit fBillToQmlDone("Done");
-}
-
-void Database::populateSvdEmailBox(){
-
-    //QSqlQuery popSEmailQuery(QSqlDatabase::database("conn4-popSEmailBox"));
-    //popSEmailQuery.exec("SELECT username FROM sender_email_info WHERE id > 0");
-    //qDebug() << "Pulling from database to populate email box............";
-    //while(popSEmailQuery.next()){
-        //counter++;
-        //qDebug() << counter;
-        //_emailAddress = popSEmailQuery.value(0).toString();
-        //qDebug() << _emailAddress;
-        //emit dbSvdEmailToQml(_emailAddress);
-    //}
-
-}
-*/
-
-/*
-void Database::getFirstDatabaseEntries(){
-    QSqlQuery fEmailQuery(QSqlDatabase::database("conn5-fEmailBox"));
-    fEmailQuery.exec("SELECT username FROM sender_email_info WHERE id > 0");
-    qDebug() << "Pulling from database to pget first entries............";
-    while(fEmailQuery.next()){
-        //if(counterTwo == 1){
-            //qDebug() << "Done.. first emails.. emitted signal";
-            //emit fEmailToQmlDone("done");
-        //}
-        //if(counterTwo > 1){
-        //Still send the others for now. I don't think this funcction is needed now. Just delete the first duplicate entry
-        counterTwo++;
-        qDebug() << counterTwo;
-        fEmailInDb = fEmailQuery.value(0).toString();
-        qDebug() << "Femail: " + fEmailInDb;
-        emit fEmailToQml(fEmailInDb);
-       // }
-        //counterTwo++;
-
-    }
-    emit fEmailToQmlDone("done");
-}
-
-//Receive and store data from the QML settings form. This is a sending email address that the user wants to save. Only Gmail is working for now.
-void Database::receiveEmailInfoFromQML(QString uName, QString pWord){
-    if(uName.isEmpty()){
-        return;
-        qDebug() << "Empty string in receiveEmailInfoToSave() function";
-    }
-    iUsername = uName;
-    iPassword = pWord;
-    insertEmailToDb();
-}
-*/
+a0f37ab2bb185c8781f0e1d339
+f9672a89c2f404d16d5b66959fbb363b
+ccba3e430ab5e0f2a0c5bf1e8d8ca500
+ebf8c261a4de428a6ff172abd383ddff
+d1bbad12cdaa66d6d0de77bf9ff5a79f
+330016c81e000219ce825be6c06f8f43
+61d562b7090d727ed05a51fb89a966c6
+3bde9a1963a93cb8cb12827c7774a3f1
+7ca908d90268f0e87bdc1d9a48939395
+93e640b2ea3f4b834b75e1a0aca68708
+e7525304ff683b5a97f5ceb5ff37659f
+d71fbf555b2bdc48fe34fa713faf4f2a
+b2307e7cbf82257e38aa9f9e73b026bc
+859b78105d7541e441e5e6a5a2138ba2
+9b17613e7a73df2e37084864c966e7d6
+87cacd2b67973f028b0653de66778b36
+9f99d4c7d36cb57a36a886415ba1e019
+4ff3d397309bc8c172f9b5dc799fa935
+dd1b6167fbc6d8395c1f68ce11c931ac
+f88d33928bb1d17dcbd715207d31d409
+32bb925f893c1381fd81669461ff3893
+bcbb663abcaf1321a3cf166c07dba3aa
+b4d2059911876a62a5d525abe4db923e
+4a22baeb78b0538c1f6173394cd2132d
+53219ae8762f0c27879d695128e0e8ea
+1ff74e8c52d797ec1bd89d62001429e7
+55e16a62c5a7cbccf8792c087e2d0de7
+8f8578acf348f058b5fced9a4b1570f3
+c3513b6bef44ad5d73e407e8c639d67e
+98a46b97ba8c347b7a1966c2e70e4872
+6a430592d11ceafd450d94d6dea6f7c2
+2da7eb09afdf977468086bd87df76f2c
+20f8229d2a5e03860a6876b2a83441bc
+068b88d99f5ddf348f1f3deac1b3a7e4
+10401d73802f684f8a064437f835d17a
+3de2be4494ac7069970e3806f2f90435
+25c98f5d2f0fa5ede24e612fb97d586a
+32658d074a77ed3411f02abf1bbe0d0a
+9f1fee186eb10269f2e21b3204620c0c
+d1de2740a6a9b7fcb6dc370582f5acfe
+08e2303eef03e91b101a860d5b1d7da1
+6c445ba713d4bf1b6d44d74340febf42
+02acb6c127438b379f57245457edbc78
+67c7b35de557a1343067a1ab307e948b
+030e57595ad4a8c56f2ea088d2a83f05
+f45f985cc4d00cacd48a4b44d011642f
+f0792fdd280fd8463a4b590ccd289bae
+87d74dbe19bc4a8e80c6389b834511ac
+879315164784ed05b3cd4c3c0399fb96
+4d6f3ca0f6bd90b726740dc02c2f55e5
+93efe7436b62be3f372de52be781225d
+f45a86140304331afa830a2671a33588
+e4fd6135887a2c75c79ddf44ee0a3cf8
+31d73a3e6c7e0c3fc938bd001f3bcc1e
+16dd50df867506fadbb6caa30becd28d
+a310e2f15eb356301451d3ef8cdaddf9
+30b1b69bcde575aa23b25d4a21a85106
+f4e70542251a5b4993ed34cf7a63ad24
+d098d2597cc8861995642078943f8dcb
+cf034f49aa6c7a8077e416e80484dc22
+126581c12956116ec720823204fca3f9
+f8cd059338b324e6ea77418081b5db19
+46d41ed087cfb848ce2e2b1543f69a22
+678cfeebefa93ee1fef3f95762f1fdb0
+47a1043049355b233e6eb66d74fb9a66
+2d10364c0a1a9ad4f17bf0c6d7590dc3
+1e1b8da7694e8900d2e289fd4592a7dd
+05246f361723fa0a9d6b0414d7c9656b
+31e599586d2ea9735568c1cb4da6392e
+76261135c207b680ab2a079cbd7c571e
+231b2e2e6df99074fc8604d3fb44814a
+d610ad2a3fb1300304312cea793b97e1
+36aecb556edd87ff9f0fd50409adc866
+b2ae9266eb13c218b439fa4e5c8012a1
+9e57a4ca40e0fcaef6551fc763d227d2
+47087f74e87019e45bfeaa19de4f5007
+7946d2a271f80d0c0b3e84291cd0bd6c
+67b52e511d5b628b70e7383f3a315693
+b0c1d9ed0eb95014a6711f5ac3bd0b36
+4a9c0dd95a98314f3cc4700095dc6307
+fcfc918cc8f58d03d408b54edad38bc7
+fc578a705c563485e93c78f9613ac4dd
+67f285a7fd072f120a654dd7c20a5943
+09d6391cc46815b63b697263121a3a56
+c54f3689bbf3d4b0d374a73926102224
+060571574e2a606866aa2a5b592542d3
+2423cf34e6b51db259c57ddc7d271fe6
+91980b61931819fde311f334e4c7a174
+07e9a2e7c3114ad4c048631398a53651
+f07a660b5087013200fae3705c47dc3d
+4a1925353cdc599cd54ad5192221a167
+fe406e20f2a1c5e8af048a9e6aea71a7
+6de2c2ea8852187552bf86a411606185
+ad7658619f5ff73acc61614f3eb33962
+4e5e9c73e96b7b34e27b2657cb19c8ed
+f4dff4b4894d8c0ea5ef85cc5a666cfa
+91759dbb69edc62e129a8bfbe87b2472
+6c04c93ff46e2cebb2515b2c6a5927c3
+d47fa9d1a28d8a713e265320730253d2
+994879414adb5686f7923bfb61ec4f3d
+52eff5adf4303b4a357e66867a338a20
+709eb1bc47b22fa1029876bf85ca3474
+6f3667a36ee9642a35f3fdd9ce0fc2cd
+01d12b54421f6f45c2ea9103b9ebe1c2
+6f174d626e867103ce347aa92f7879d8
+e82a3e4579b535fe19adf7bb8ff174dc
+2e84593bc85935dd3111878e57cb45bd
+1718cc62e1352e2dd7c50ed8c7026d0e
+cd01fbe8cb235fa929c962eb1a677670
+cd26343feae1a1ba826688a37ee61a5c
+fcec39fb99dc6fbc81aea9b64f4014b8
+2831077c4819570ffa1c7fb9b351806a
+a317a348079f963b7a9f43673932391b
+0e837d9761bc07efe6b2638973a52dbb
+df725124d90832072f9f32dd07187941
+13562380d2d89c01bc23af60844c66f8
+383231b4537af2365e63844c642f6968
+d36c342126c2f229c91b479fa63f5b96
+0b0c58071afee542f4a7d530493dc170
+6fbc89f36a5ab5fdff602f3124838814
+6b9d9ab02d72fda18c9864b8108e28c4
+e83154d89f25726788c1fd15bb765c8a
+dde67badd6608122362bb86e9f4c7775
+af044875e1b1bc217e57c2f5ac931d0b
+4a2c71bf1525220d8a6476d1c22bc5fe
+49ea7c490c9a0d5d066687ca208b1ff8
+18e2a6948a858aeda246cef1419ad330
+0567409350f550424520aec564fc86d3
+1beef1d699dbe18117cef42eea71eb06
+8938e59442ed68a6259827f6da204f7e
+54d413e44d9e51adb136a49a582e8e37
+009bb561aa3b98e0224381237375e067
+eb963509cc568da0817fac8b8b81d874
+b9bfa02a88d769a4cadeedbee8619311
+0c082a04b849750c147382c21e68f55f
+c00aff498e3b5011f3ce273ecad7858d
+c96900444db85dce9a4c3bb74909fa14
+7f2ec623fa2ee03803b8a6b857501c3c
+d6c52a2eccc279e95f74319f16114dd0
+7fda37f6edb80f6971ba12deebe85a83
+75c2d1e676514283eba9936951f1c68c
+b3e4cff6fee1e5bc9758c96a0a7af432
+97f033330797446ac308f2ef5b3c8450
+ac18d01e289c97c3f4401349f12b3180
+ce615c2be1a4f9dc13d3011b5e08cfc1
+3d240ce5e7a1dcb918343b16ca554009
+0961413ba12872d76e0578c601f07932
+069a111bcdd068bc63a759c6414a5bd5
+510ac04f0ade26a018b947f584b09ff2
+26f7f2ba883758525074c78a660c0eb9
+5643df49fc9463140a72b89d53edefce
+70a498cda2caeb20e6b01b018d222d4a
+5dfd7adbada09cfba6a7295615770a63
+11f5f1cd2c092edb9863c8db9fbeaca5
+f63d9b56fa78b243a88f4da38b0b0242
+1ff30ccea75766287997af5b30e496fa
+527125f2e1d13bbec33d197ea89f0aa3
+84429830fbab42873157023082c04090
+4da4fd7c7856457d5102279cb7fa753b
+4952a8c89459ff6ea17038cc8e00426f
+2f7ce81760943c3c6e3ddeddb9f4dcd8
+4918d92d52bbf375e57195b886e952d2
+227ea7ee5efe72b3dd8db4e1c0367f0d
+6eaaa61bb2ae3d18d0d1279457ef8eee
+5f721cf2b9aef8f203bb728d9bce8bd9
+29c028a9a0af2ab3f0e14453394ff103
+a04d549427a4474f4936be14cf25d26e
+d0f62bd0b21909fda6d4f481123fdd54
+3cf524f387b4a61d77809c93788c4f02
+4c68b38536036652f688e4d6b8684934
+c615daf3caff85940efbec650b2681fc
+cb17c3cdd776338620e7c1eb595e30a9
+7042f3f02814b876b828a4d3a57db1f1
+f7d61a7169ff9940f5ada48f5259c101
+68f528629b8c44a0aa42013adac12c3d
+7799734e0b3e092be04651b020e02bfa
+d3ac1f74f790359f685d3ac36d07a4fc
+754920a0bb83cfc29392857a52f052eb
+37ea1da8b4a5550b9848189133d337f4
+4fad53292bc4aa8dcceb705cf12e14ac
+28b3ae223a7b3c68bfe5032916d728d0
+175bfc27ca50e42b09f87c36fe4088ef
+c078e6e3e59202e1a12b014ebc3df082
+f3f17f16b680af0363fa562292da2a63
+2c8626eb28cf254e283736dac8fd0a5a
+227b7c0d1780aa6247e86158a33ceb30
+6b14f7884ebbc77cf63afb1c60135fc7
+ca0e25fcadd1aefa2db25d24c30260b8
+4f488f97af88a5078c76e68bf29a0cdb
+c7e2bd27954723803042edd678cbfbb9
+47214d1e199f143f7792fccc60cabefa
+06358ebf4875789e1743ceec1ac4705e
+41d693a922278c56fa7bd7d6b4b4a508
+d5d79658480b535ea1f96c7e8b703996
+7d70255f56097a4c7af64aa42d023462
+5142d689439ce207767d55be269830a7
+649de20c66fc0e339b8421aa2473e738
+1604924ec2d305c25562fbc7bc358b12
+32fa74b6f1e78818d6da0dc516146b2e
+d4a2712641121ddf76487e899bee8893
+1de7f5f6458bd8b8beace254385c3535
+677e10ef1291339e2e257fac54be1075
+f2361c766b60a9cbc0d4a2062a917e94
+6ba9e36091341c3cc43dbe22d1b94f73
+738b16e694df0e0410557b5d7bf0dcaf
+a3d3fd8bb93b127b95de525328b8b329
+c877a60c9130a014e1971ce0938b4a2c
+37dcb88d77a78c96c9c121a11f37614f
+8ce57ca10f0b1b0f26fba5cb4d4315c1
+bb608d44284f8ee30af23cb6334e48e5
+f11c8269f54acc7b26c1ca3aecb1613d
+cb63cde2691f86f87cecdc436b70fa82
+56699637eb858fafcf34169cf9b7e6fd
+7aff9843125b2a3de505f63a143ce434
+13f5bebaa9450e67df1e7aa36042cf75
+555e9128ab7376cf1beab241b20fba17
+3a059fd0bf6adcff88a8ba3388e11c89
+e95bb976665203e4baa86aa64bd259cb
+1fd00bd94be96163ef5ce7618f51ae67
+3ad23c63dffa178654add6a1f23200c2
+ecc1381c0802657768d51ee900140a8c
+471a7497d63c6c35db0f8e538368ebba
+4a91312ec5b61e536001412f42d2ce59
+039ba7559925cd5d833432ad4a1ad20a
+7d2bc59b172817e6377d8c1f4ff47a08
+0a7c65ad93d2e777458d3ad3b0e9c490
+b81be1c954a429e8b0cd8ed8daca5c50
+303b8a530206735cca48953a8909f84b
+1c0c8d3d1db10f82c9d4698efa137b1e
+7b0b6f8991ab759548b2caaa31b6bbc3
+efe34d59fe4d63c16eb1058ea4bb01d2
+12ed93524b8ad8a4b0b855efc99d802f
+271d558aeaef1b56ef56557cfae4e8da
+015bfd845b7f0b15358d41ecee6278b2
+2b942a59a6cfb975b474f665020582a7
+3ba7c6c40112bdfd3208f27c5a392156
+1cefed4a69b49078b059a5390170676a
+bc041d60e624d335874a792956f70c92
+36b4b1204f017e4b1327485292c1ee57
+cc59dac78cb7fc18558400e6b8dca26b
+7d3f9ba64508cf04adf534dc8bfebc41
+09c52bcc7e46323507dcfa195e617991
+32c668b022525b6877ed919404d57679
+7ee1df96d9b7eae4ead2cca3863f0d40
+13c3fe6dfb3061372e8dd6119ea2bc36
+44c0c5fd8c48272652ec3c0f4ab279f8
+fdf5712f13a909bc06b20140040fdb38
+5ad231d006039bf39e97e50587ba0c37
+13ce5dd4874d9e9ca849d119afdea8a6
+3dc9442462863aeca323a5e77d12a43b
+5cf89a5510c968c8aa8d0cd0dfb5d84e
+3f71317da2163acdc552272a4ae11549
+433e91c4e0ac15d31acd0f62808ba30d
+1570ecb3395d475473a3455e6061fe4b
+3470a3390e3dc88c135c5d005509484f
+9285df489572b8825306f40063b655a7
+ab4acfc4c0b3725d4924fb0f56c76510
+0d75843b9d700ab43e45bae715ee3022
+1959c94f487d8fea4a3139fedb7dcdf3
+92fff66f0f2766adb84209a975cf4f31
+e8cf433b8cf88e18d529450291b74a7d
+e625ab996f3e847565fefdfc097d84da
+4a0ba746055ac1dbb166685c9fea0513
+0172029337bfc92a24511798dd98f242
+bad9ffe72677944f4293dcdfc8c3de32
+32e928060b449b4f404ed93a784030e0
+0d20739cfd6d6f986c4d77848edc61a3
+79d9908b6769e64f922e74a090f5ceeb
+50724ff69429c7776eb39742714c3891
+4d7ffbc4648c9377b6fb16d65c08cf1f
+50d49664b738d0044543f7ebb96f960d
+4894c2506436f5f28ef62e50c1677244
+625c186240472f47b30d8b3885443d72
+04629a7f751859616b58dddc67c18618
+90c00ed25b2f0997a13ac5531a057afa
+f44cec5a52162bc17010e63133cdf71c
+c142cd7dacd8a15ee334256bd4628fc3
+a2f8d6f625e9e257c2995597421e4d94
+29745fcc180b948a45c2c4e5247a04b7
+8a84b1a6bee31d0aa36956803f3952dc
+f170c31f6be1727b72819c1c6533c098
+2d7bc7895e0b6dc8f92a0c3d702ef303
+26ecd9168a5e2d15990602ce25f26767
+6459e09db0762016c0628724cbaca66e
+83ae6b7d730acf0fa614eaf2bd805b0e
+28f3689884a0691c2a9e787e03bcd3f2
+caf79999974c10cbbe33c2b8e25523ef
+6c8db750c5087d43749635aeccb355ca
+06e178a29f5addf14142a6c2e7fe9147
+8a53e48a950c360a8d76389d2f5b5909
+13a74debe47b60011b9b7fc36b46d0c9
+19d7b20774d5f87483be01920cb5a724
+0694418b2250da68e2f4bfff339defbe
+288c854afb4613dcb0a71643dbf2e5f4
+eddfb549f3a8cee775efc65b55e4ba4d
+bc46a41ece664cc506fe52c8e382a93c
+18484945dd7ee2f88adac070865a7806
+ab81e56ac786c5c1fbb9f19ee8a2fe1d
+4642edf35668a3c1920fb7963a140480
+bd3667d7e307143578b8aa163eb8511c
+4cba31730b7d67d7f36692154a3f5124
+fe93fcadfd4744495e109e685d997785
+a66d463b0309b18824a49ee8c8a765ab
+aeba186388fb048857872105b0761f5a
+73ea8a8e13acef47f4c90b774b667744
+183ffde82545cd7bfa378c99abd785f8
+9d2884771ae06bb040a0e3053a60043d
+d4b13233a88b2c25700bf87e4a8aba6c
+2bb11bedec4d2f1b47da3e7c201ddbf5
+044734e4da5843b6e339c195bb60e815
+893fedf0aae3be1e22c8c35efcca2fca
+bb9e17c2a774d9b4cb3313e6a363981d
+d01a01f0376e77b1b5b98b33b955c2ee
+4870ed85835d47ecd333c1fe61ec3237
+71ead381425db504590dc515ce4ea4d6
+3fc2b7dca4ca875604b728585bedce20
+621b52259d6873994e7b92a56cf34c0a
+c37af01abe9ea2b8b3463db7ddfc5303
+107353c0dbe1a97adfa2e97b825caf57
+aa93bc43faf4ae6fb61b1ad09afd4934
+9e44600e526a869eb0da3c5ee1b6efd8
+91fff850d98524867bab946fb754325d
+8bf46f8ca259f9d5b1ca59f84a25261a
+8fa587a13f1c0a6630855b0c89319ea4
+1e4275f017851e23d3ec9616e8a54bc2
+36157f2b9995b5448b537fd102554245
+082ad79488db1c50260109c011fe8035
+06723e015880985698ea5f824a2e8385
+7d8c5da97d6b6ccd371fc9c65304f6e7
+22f60ea86d2d6e71b9790616a4ff54f9
+b76ed1157e17fabaab7d16dc87aa4a31
+8949dce3ae80b54b6c65f2d176bda1f1
+169b53ff05ba947b39be98cd7ff3e141
+247e2af4dbfc487b3e7575259cfbdc0f
+da084fb395f699e39e3c30a861a393fe
+893f357f4665f11bbc1
